a00f3614e734f8ed1ce9fb42ea0177ba261a7aea
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-30 22:40:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    var coins = {H: 50, Q: 25, D: 10, N: 5, P: 1};
+    var result = {};
+    if (currency > 10000){
+    	return { error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    for(let key in coins){
+    	const count = Math.floor(currency / coins[key]);
+    	if ( count > 0){
+    		result[key] = count;
+    		currency -=  coins[key] * count;
+    	}
+    }
+   return result;
 }
